f78c03c0b071b9b00fdcc6d55eba9f56163e469d
Vladimir Arkhangelky <arkhangelsky@t-logika.ru>
2018-09-17 20:37:12 +0300

    Creating source files for a project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+#include "stdio.h"
+#include "menu.cpp"
+using namespace std;
+int main(int argc, char const *argv[]) {
+  int inp = 0;
+  system("clear");
+  mainMenuPrint();
+  cin>>inp;
+  switch (inp) {
+    case 1:{
+      
+    }
+  }
+  return 0;
+}
---
+++ menu.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+
+using namespace std;
+void mainMenuPrint(){
+  cout<<"Просмотреть таблицу"<<endl;
+  cout<<"Добавить практику"<<endl;
+  cout<<"Удалить практику"<<endl;
+  cout<<"Редактировать практику"<<endl;
+  cout<<"Выход"<<endl;
+}
+void printTable(){
+  //todo:print table
+}
